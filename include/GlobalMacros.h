b1bfd465cbe7e664cacfb83275ba4965d857a6b9
stuartstaniford <stuart@earlywarn.org>
2020-09-24 11:55:40 -0400

    Added GlobalMacros.h

---
+++ include/GlobalMacros.h
@@ -0,0 +1,13 @@
+// Copyright Staniford Systems.  All Rights Reserved.  Sep 2020 -
+// A place for macros that we want to be able to use in many locations.
+
+
+#ifndef GLOBAL_MACRO_DEFNS_H
+#define GLOBAL_MACRO_DEFNS_H
+
+#define unless(X) if(!(X))
+#define epsilon 0.000000001f
+
+#endif
+
+
