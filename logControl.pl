70620d0a2e3c5dec18d09b3a152ea238dd90c45d
stuartstaniford <stuart@earlywarn.org>
2021-05-01 13:01:26 -0400

    Start logControl script.

---
+++ logControl.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+# Script for tweaking the logging options so that permaplan can be compiled with a specified
+# logging set-up for use in automated test scripts where we need to control what the logging
+# options are so that we can check correctness based on log output.
