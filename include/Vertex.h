c388c4941102b4a003d5e7db901cda8ec68d3e08
stuartstaniford <stuart@earlywarn.org>
2021-07-07 18:44:22 -0400

    Partial doxygen comments for Vertex class.

--- include/Vertex.h
+++ include/Vertex.h
@@ -15,14 +15,14 @@
 
 
 // =======================================================================================
-// An Enum for how much of the Vertex to fill out when returning from things like
-// getNextVertex.
+/// @brief An Enum for how much of the Vertex to fill out when returning from things like
+/// getNextVertex.
 
 enum VertexDetail
 {
-  PositionOnly,
-  IncludeTexture,
-  IncludeNormal
+  PositionOnly,   ///< Only provide position vectors
+  IncludeTexture, ///< In addition, include texture co-ords (but not normals)
+  IncludeNormal   ///< Provide everything including normals
 };
 
 enum VertexDrawType
@@ -44,11 +44,13 @@ inline bool mollerTrumbore(vec3 triangle[3], vec3 rayPos, vec3 rayDir, float& ou
 }
 
 // =======================================================================================
-// Used as the elements within a buffer that will be passed to the shader
-// Note: see here for discussion of half precision floating point, which is key
-// to keeping our vertex type at or below 32 bytes (1/4 of a cache line on many
-// architectures).
-// https://clang.llvm.org/docs/LanguageExtensions.html#half-precision-floating-point
+/// @brief Class for storing information about a single vertex.
+/// 
+///  Used as the elements within a buffer that will be passed to the shader
+/// Note: see here for discussion of half precision floating point, which is key
+/// to keeping our vertex type at or below 32 bytes (1/4 of a cache line on many
+/// architectures).
+/// https://clang.llvm.org/docs/LanguageExtensions.html#half-precision-floating-point
 
 class Vertex
 {
@@ -64,6 +66,11 @@ class Vertex
 #endif
   
   // public methods
+  
+  /// @brief set the position of this Vertex 
+  /// @param x - the X value of pos
+  /// @param y - the Y value of pos
+  /// @param z - the Z value of pos
   inline void setPosition(float x, float y, float z)
   {
    pos[0] = x;
@@ -71,22 +78,31 @@ class Vertex
    pos[2] = z;
   }
 
+  /// @brief set the position of this Vertex 
+  /// @param v A vec3 which will be copied into the position field
   inline void setPosition(vec3 v)
   {
    glm_vec3_copy(v, pos);
   }
                            
+  /// @brief set the texture coordinates of this Vertex
+  /// @param s - the horizontal texture coordinate
+  /// @param t - the vertical texture coordinate
   inline void setTexCoords(float s, float t)
    {
     tex[0] = s;
     tex[1] = t;
    }
 
+  /// @brief set the color of this Vertex
+  /// @param u - an unsigned interpreted as a four byte RGBA array.
   inline void setColor(unsigned u)
    {
     color = u;
    }
   
+  /// @brief set the color of this Vertex
+  /// @param u - an unsigned interpreted as a four byte RGBA array.
   inline void setNormal(float x, float y, float z)
   {
    normal[0] = x;
@@ -94,6 +110,11 @@ class Vertex
    normal[2] = z;
   }
 
+  /// @brief set the normal vector of this Vertex
+  /// @param n - a vec3 with the normal direction.  Note, although supplied as a
+  /// float vector, it will be converted to half precision interally in the vertex 
+  /// structure.  Note also that normals will be normalized in the fragment shader,
+  /// so it is not necessary that they be a unit vector here.
   inline void setNormal(vec3 n)
   {
    normal[0] = n[0];
@@ -101,6 +122,8 @@ class Vertex
    normal[2] = n[2];
   }
 
+  /// @brief Store the low 16 bits of an OID in the Vertex.
+  /// @param oid - an unsigned objectId
   inline void setObjectId(unsigned oid)
    {
 #ifdef LOG_VALID_TRIANGLE_BUFS
@@ -108,12 +131,16 @@ class Vertex
 #endif
    }
   
+  /// @brief Print the vertex on one line of a file with tab separated fields
+  /// @param file The FILE* pointer to an open file.
   inline void fprint(FILE* file)
    {
     fprintf(file, "pos:\t%.1f,\t%.1f,\t%.1f\ttex:\t%.3f\t%.3f\n",
             pos[0], pos[1], pos[2], tex[0], tex[1]);
    }
   
+  /// @brief Copy another Vertex to this one.
+  /// @param src A pointer to the other Vertex to be copied.
   inline void copy(Vertex* src)
    {
     memcpy(this, src, sizeof(Vertex));
