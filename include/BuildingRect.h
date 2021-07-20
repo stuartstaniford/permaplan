1bc31d9846b577465be3032006ecc81260d8ca38
stuartstaniford <stuart@earlywarn.org>
2021-07-20 05:15:06 -0400

    Stub out BuildingRect

---
+++ include/BuildingRect.h
@@ -0,0 +1,40 @@
+// Copyright Staniford Systems.  All Rights Reserved.  July 2021 -
+
+#ifndef BUILDING_RECT_H
+#define BUILDING_RECT_H
+
+#include "VisualElement.h"
+
+
+// =======================================================================================
+/// @brief An element of a larger structure which is rectangular.
+/// 
+/// This class is used extensively to represent pieces of buildings, such as a wall or
+/// one plane of a roof.  It provides for keeping track of its orientation and size
+/// relative to the larger structure, and for keeping track of texture.
+
+class BuildingRect: public VisualElement
+{
+public:
+  
+  // Instance variables - public
+  
+  // Member functions - public
+  BuildingRect(void);
+  ~BuildingRect(void);
+  
+private:
+  
+  // Instance variables - private
+  
+  // Member functions - private
+  BuildingRect(const BuildingRect&);                 // Prevent copy-construction
+  BuildingRect& operator=(const BuildingRect&);      // Prevent assignment
+
+};
+
+#endif
+
+
+
+
---
+++ src/BuildingRect.cpp
@@ -0,0 +1,26 @@
+// Copyright Staniford Systems.  All Rights Reserved.  July 2021 -
+// This class is used extensively to represent pieces of buildings, such as a wall or
+// one plane of a roof.  It provides for keeping track of its orientation and size
+// relative to the larger structure, and for keeping track of texture.
+
+
+#include "BuildingRect.h"
+
+
+// =======================================================================================
+/// @brief Constructor
+
+BuildingRect::BuildingRect(void)
+{
+}
+
+
+// =======================================================================================
+/// @brief Destructor
+
+BuildingRect::~BuildingRect(void)
+{
+}
+
+
+// =======================================================================================
