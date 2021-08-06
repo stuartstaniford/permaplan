8cce3ec1c3a1c20dfcb08d33b2619c2bc7b7d546
stuartstaniford <stuart@earlywarn.org>
2021-08-06 04:58:17 -0400

    Few more comments in VisualLayer.

--- include/VisualLayer.h
+++ include/VisualLayer.h
@@ -5,16 +5,20 @@
 
 
 // =======================================================================================
-/// @brief This class is for modeling information that supports the visual layer system
-/// (allowing certain things to be turned on and off in the renderings).
+/// @brief This class is for modeling information that supports the visual layer system.
 ///
-/// More details of the class go here.
+/// The purpose of the layer system is to allow visual objects to be classified as to the
+/// kind of thing they are, and then to be turned on and off in the renderings based on
+/// those classes.  A few prepopulated layers will be supplied, but the intent is to 
+/// allow the user to also create custom layers.  Layers are implemnted as a bitmap, and
+/// thus are currently restricted to 32 total layers.
 
 class VisualLayer
 {
 public:
   
   // Instance variables - public
+  unsigned layerFlags;
   
   // Member functions - public
   VisualLayer(void);
