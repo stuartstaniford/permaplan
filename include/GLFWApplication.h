4022afdf990b41d3349c59cbab264600f8d0ca7e
stuartstaniford <stuart@earlywarn.org>
2021-09-15 06:01:26 -0400

    Add GLFWApplication.

---
+++ include/GLFWApplication.h
@@ -0,0 +1,39 @@
+// Copyright Staniford Systems.  All Rights Reserved.  September 2021 -
+
+#ifndef GLFW_APPLICATION_H
+#define GLFW_APPLICATION_H
+
+
+// =======================================================================================
+/// @brief Overall organization of an application consisting of multiple GLFW Windows.
+/// 
+/// This manages the overall rendering and simulation loop and keeps track of which 
+/// windows are currently open/on-screen.  It's very specific to OpenGL/GLFW.
+
+class GLFWApplication
+{
+public:
+  
+  // Instance variables - public
+  
+  // Member functions - public
+  GLFWApplication(void);
+  ~GLFWApplication(void);
+  
+private:
+  
+  // Instance variables - private
+  
+  // Member functions - private
+  /// @brief Prevent copy-construction.
+  GLFWApplication(const GLFWApplication&);       
+  /// @brief Prevent assignment.
+  GLFWApplication& operator=(const GLFWApplication&);      
+
+};
+
+#endif
+
+
+
+
---
+++ src/GLFWApplication.cpp
@@ -0,0 +1,24 @@
+// Copyright Staniford Systems.  All Rights Reserved.  September 2021 -
+// This manages the overall rendering and simulation loop and keeps track of 
+// which windows are currently open/on-screen.  It's very specific to OpenGL/GLFW.
+
+#include "GLFWApplication.h"
+
+
+// =======================================================================================
+/// @brief Constructor
+
+GLFWApplication::GLFWApplication(void)
+{
+}
+
+
+// =======================================================================================
+/// @brief Destructor
+
+GLFWApplication::~GLFWApplication(void)
+{
+}
+
+
+// =======================================================================================
