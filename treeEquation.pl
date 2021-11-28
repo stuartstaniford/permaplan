c76768a9cd065dc0025e115c165891491c62f5d5
stuartstaniford <stuart@earlywarn.org>
2021-11-28 08:57:53 -0500

    Beginnings of parsing of raw data file.

---
+++ treeEquation.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+$rawTreeFile = "ForestServiceUrbanDatabase/Data/TS3_Raw_tree_data.csv";
+$coefficientFile = "ForestServiceUrbanDatabase/Data/TS6_Growth_coefficients.csv";
+$outFile = "ForestServiceUrbanDatabase/json/trees.json";
+
+open(RAWFILE, $rawTreeFile) || die("Couldn't open $rawTreeFile.\n");
+open(COEFFILE, $coefficientFile) || die("Couldn't open $coefficientFile.\n");
+
+open(OUT, ">$outFile") || die("Couldn't open $outFile.\n");
+
+$firstRow = <RAWFILE>;
+$firstRow =~ s/[\r\n]//g;
+
+@names = split('\,', $firstRow);
+
+print OUT "\[\n";
+$firstLine = 1;
+while(<RAWFILE>)
+{
+  if($firstLine)
+   {
+    $firstLine = 0;
+   }
+  else
+   {
+    print OUT ",\n"; # terminate the previous line
+   }
+  print OUT "\{\n";
+  s/[\r\n]//g;
+  @vals = split('\,', $_);
+  $firstVal = 1;
+  foreach $i (0.. $#names)
+   {
+    if(defined $vals[$i] && $vals[$i])
+     {
+      if($firstVal)
+       {
+        $firstVal = 0;
+       }
+      else
+       {
+        print OUT ",\n"; # terminate the previous line
+       }
+      print OUT "\'$names[$i]\'\:\t\'$vals[$i]\'";
+     }
+   }
+  print OUT "\n\}";
+}
+print OUT "\n]\n";
+
+close(RAWFILE);
+close(COEFFILE);
+close(OUT);
