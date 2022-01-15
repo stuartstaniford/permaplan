17a374cffb0348663d96beff83ec00dbe449c30d
stuartstaniford <stuart@earlywarn.org>
2022-01-15 08:50:19 -0500

    Stub out HttpLBPermaserv.

---
+++ include/HttpLBPermaserv.h
@@ -0,0 +1,40 @@
+// Copyright Staniford Systems.  All Rights Reserved.  January 2022 -
+
+#ifndef HTTP_LB_PERMASERV_H
+#define HTTP_LB_PERMASERV_H
+
+
+// =======================================================================================
+/// @brief This is a template for new classes.  Quick description of the class here.
+///
+/// More details of the class go here.
+
+class HttpLBPermaserv
+{
+public:
+  
+  // Instance variables - public
+  
+  // Member functions - public
+  HttpLBPermaserv(void);
+  ~HttpLBPermaserv(void);
+  
+private:
+  
+  // Instance variables - private
+  
+  // Member functions - private
+  /// @brief Prevent copy-construction.
+  HttpLBPermaserv(const HttpLBPermaserv&);       
+  /// @brief Prevent assignment.
+  HttpLBPermaserv& operator=(const HttpLBPermaserv&);      
+};
+
+
+// =======================================================================================
+
+#endif
+
+
+
+
---
+++ src/HttpLBPermaserv.cpp
@@ -0,0 +1,23 @@
+// Copyright Staniford Systems.  All Rights Reserved.  January 2022 -
+// This class ..... is a template for real classes
+
+#include "HttpLBPermaserv.h"
+
+
+// =======================================================================================
+/// @brief Constructor
+
+HttpLBPermaserv::HttpLBPermaserv(void)
+{
+}
+
+
+// =======================================================================================
+/// @brief Destructor
+
+HttpLBPermaserv::~HttpLBPermaserv(void)
+{
+}
+
+
+// =======================================================================================
