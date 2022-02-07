stuartstaniford/permaplan

commit 5805aa99a53e44789799bd4a71a7b4bbf51bf9c2

Mechanism to pick up when an HttpServThread is calling for shutdown (doesn't work yet).

include/HttpLoadBalancer.h
@@ -19,6 +19,7 @@ class HttpDebug;
 class TaskQueueFarm;
 class TaskQueue;
 class GLFWApplication;
+class HttpServThread;
 
 
 // =======================================================================================
@@ -43,8 +44,8 @@ public:
   void* processConnections(void);
 
 protected:
-  TaskQueue**         httpThreads; // must be initialized by subclass that knows the real type
-                                   // of these
+  HttpServThread**  httpThreads;  // must be initialized by subclass that knows the 
+                                  // real type of these
   
 private:
   

include/HttpServThread.h
@@ -19,6 +19,12 @@
 
 
 // =======================================================================================
+// Forward declarations
+
+class HttpLoadBalancer;
+
+
+// =======================================================================================
 /// @brief Superclass for any thread that is going to handle the specifics of an HTTP
 /// server.
 ///
@@ -29,6 +35,8 @@
 
 class HttpServThread: public TaskQueue
 {
+  friend HttpLoadBalancer;
+  
 public:
   
   // Instance variables - public

src/HttpLBDebug.cpp
@@ -16,7 +16,7 @@ HttpLBDebug::HttpLBDebug(unsigned short servPort, Scene& S, GLFWApplication& win
                     scene(S)
 {
   for(unsigned i=0; i<HTTP_THREAD_COUNT;i++)
-    httpThreads[i] = (TaskQueue*) new HttpDebug(scene, winApp, i);
+    httpThreads[i] = (HttpServThread*) new HttpDebug(scene, winApp, i);
 }
 
 

src/HttpLBPermaserv.cpp
@@ -17,7 +17,7 @@ HttpLBPermaserv::HttpLBPermaserv(unsigned short servPort):
 {
   solarDatabase = new SolarDatabase;
   for(unsigned i=0; i<HTTP_THREAD_COUNT;i++)
-    httpThreads[i] = (TaskQueue*) new HttpPermaServ(i, solarDatabase);
+    httpThreads[i] = (HttpServThread*) new HttpPermaServ(i, solarDatabase);
 }
 
 

src/HttpLoadBalancer.cpp
@@ -17,7 +17,7 @@
 // =======================================================================================
 /// @brief Constructor for a new load balancer farm.
 /// 
-/// Creates the socket, binds to the port, and fires up a TaskQueueFarm of HttpDebug
+/// Creates the socket, binds to the port, and fires up a TaskQueueFarm of HttpServThread
 /// instances to handle future requests.
 /// @param servPort The server port to open up
 /// @param S A reference to the scene (pass through to the HttpDebug instances, which
@@ -46,8 +46,8 @@ HttpLoadBalancer::HttpLoadBalancer(unsigned short servPort):
   if ((listen(sockfd, 6)) != 0)
     err(-1, "Listen failed on socket %d in __func__\n", sockfd);
 
-  httpThreads = new TaskQueue*[HTTP_THREAD_COUNT];  
-  servFarm = new TaskQueueFarm(HTTP_THREAD_COUNT, httpThreads, (char*)"httpFarm");
+  httpThreads = new HttpServThread*[HTTP_THREAD_COUNT];  
+  servFarm = new TaskQueueFarm(HTTP_THREAD_COUNT, (TaskQueue**)httpThreads, (char*)"httpFarm");
 }
 
 
@@ -65,7 +65,7 @@ HttpLoadBalancer::~HttpLoadBalancer(void)
 
 void processOneConnection(void* arg, TaskQueue* T)
 {
-  HttpDebug* http = (HttpDebug*)T;
+  HttpServThread* http = (HttpServThread*)T;
   HttpLBWorkUnit* unit = (HttpLBWorkUnit*)arg;
   int connfd = unit->fileDescriptor;
   http->processOneHTTP1_1(connfd, unit->clientPort);
@@ -106,6 +106,9 @@ void* HttpLoadBalancer::processConnections(void)
         
     if(shutDownNow)
       break;
+    for(int i=0; i<HTTP_THREAD_COUNT; i++)
+      if(httpThreads[i]->shutdownRequested)
+        break;
    }
   
   LogCloseDown("HttpDebug server shutting down normally.\n");

include/Lockable.h
@@ -34,7 +34,8 @@ public:
   inline void lock(void)
    {
 #ifdef MULTI_THREADED_SIMULATION
-    if(pthread_mutex_lock(&mutex)) err(-1, "Lock failure.\n");
+    if(pthread_mutex_lock(&mutex)) 
+      err(-1, "Lock failure.\n");
 #endif
    }
 

commit 4377b44cffcc18d43384246ec80b3844cd99b923

Fix lock failure bug introduced in 5805aa99a53e44789799bd4a71a7b4bbf51bf9c2.

include/HttpLoadBalancer.h
@@ -44,7 +44,7 @@ public:
   void* processConnections(void);
 
 protected:
-  HttpServThread**  httpThreads;  // must be initialized by subclass that knows the 
+  TaskQueue**  httpThreads;  // must be initialized by subclass that knows the 
                                   // real type of these
   
 private:

include/HttpServThread.h
@@ -53,7 +53,6 @@ protected:
   char*               respBuf;
   char*               headBuf;
   unsigned short      clientP;
-  bool                shutdownRequested;
 
 public:
   

include/TaskQueue.h
@@ -12,6 +12,8 @@
 
 class TaskQueue;
 class TaskQueueFarm;
+class HttpLoadBalancer;
+
 
 // =======================================================================================
 /// @brief Specify a chuck of work that can be placed on a TaskQueue.
@@ -38,7 +40,8 @@ class Task
 class TaskQueue: public Lockable, public std::list<Task*> 
 {
   friend TaskQueueFarm;
-
+  friend HttpLoadBalancer;
+  
  public:
   
   // Instance variables - public

src/HttpLBDebug.cpp
@@ -16,7 +16,7 @@ HttpLBDebug::HttpLBDebug(unsigned short servPort, Scene& S, GLFWApplication& win
                     scene(S)
 {
   for(unsigned i=0; i<HTTP_THREAD_COUNT;i++)
-    httpThreads[i] = (HttpServThread*) new HttpDebug(scene, winApp, i);
+    httpThreads[i] = (TaskQueue*) new HttpDebug(scene, winApp, i);
 }
 
 

src/HttpLBPermaserv.cpp
@@ -17,7 +17,7 @@ HttpLBPermaserv::HttpLBPermaserv(unsigned short servPort):
 {
   solarDatabase = new SolarDatabase;
   for(unsigned i=0; i<HTTP_THREAD_COUNT;i++)
-    httpThreads[i] = (HttpServThread*) new HttpPermaServ(i, solarDatabase);
+    httpThreads[i] = (TaskQueue*) new HttpPermaServ(i, solarDatabase);
 }
 
 

src/HttpLoadBalancer.cpp
@@ -46,7 +46,7 @@ HttpLoadBalancer::HttpLoadBalancer(unsigned short servPort):
   if ((listen(sockfd, 6)) != 0)
     err(-1, "Listen failed on socket %d in __func__\n", sockfd);
 
-  httpThreads = new HttpServThread*[HTTP_THREAD_COUNT];  
+  httpThreads = new TaskQueue*[HTTP_THREAD_COUNT];  
   servFarm = new TaskQueueFarm(HTTP_THREAD_COUNT, (TaskQueue**)httpThreads, (char*)"httpFarm");
 }
 
@@ -107,7 +107,7 @@ void* HttpLoadBalancer::processConnections(void)
     if(shutDownNow)
       break;
     for(int i=0; i<HTTP_THREAD_COUNT; i++)
-      if(httpThreads[i]->shutdownRequested)
+      if(httpThreads[i]->timeToDie)
         break;
    }
   

src/HttpPermaServ.cpp
@@ -138,7 +138,7 @@ bool HttpPermaServ::processRequestHeader(void)
   if( strlen(url) == 6 && strncmp(url, "/quit/", 6) == 0)
    {
     internalPrintf("OK\n");
-    shutdownRequested = true;
+    timeToDie = true;
     retVal = true;
    }
 

src/HttpServThread.cpp
@@ -19,8 +19,7 @@ HttpServThread::HttpServThread(unsigned index):
                                     reqParser(8192),
                                     respBufSize(16384),
                                     headBufSize(4096),
-                                    clientP(0),
-                                    shutdownRequested(false)
+                                    clientP(0)
 {
   respBuf = new char[respBufSize];
   headBuf = new char[headBufSize];