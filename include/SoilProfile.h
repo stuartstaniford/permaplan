b8a0bcd11d403a41fee4fff88588314ea61fe5e8
stuartstaniford <stuart@earlywarn.org>
2022-02-17 08:53:16 -0500

    Stub out the SoilHorizon class.

--- include/SoilProfile.h
+++ include/SoilProfile.h
@@ -3,6 +3,20 @@
 #ifndef SOIL_PROFILE_H
 #define SOIL_PROFILE_H
 
+#include <vector>
+
+
+// =======================================================================================
+/// @brief POD Helper class for a single soil horizon in a soil profile
+///
+/// This class represents one unit of soil in a soil profile (eg taken from some database
+/// or soil survey map).  For example the topsoil (or "A horizon") might be an example.
+
+class SoilHorizon
+{
+  
+};
+
 
 // =======================================================================================
 /// @brief Model the nature of the soil at a given location.
@@ -14,7 +28,7 @@
 /// modified by local on-the-ground observations, interpolation operations between
 /// neighboring points, etc. 
 
-class SoilProfile
+class SoilProfile: std::vector<SoilHorizon>
 {
 public:
   
--- src/SoilProfile.cpp
+++ src/SoilProfile.cpp
@@ -6,6 +6,14 @@
 // modified by local on-the-ground observations, interpolation operations between
 // neighboring points, etc. 
 
+// For US soil mapping info, see:
+// https://websoilsurvey.sc.egov.usda.gov/App/HomePage.htm
+
+// Helpful information on the structure of data in the World Soil Information Service 
+// (WoSIS) database:
+//https://data.isric.org/geonetwork/srv/eng/catalog.search#/metadata/ca880bd4-cff8-11e9-8046-0cc47adaa92c
+// https://essd.copernicus.org/articles/12/299/2020/essd-12-299-2020.pdf
+
 #include "SoilProfile.h"
 
 
