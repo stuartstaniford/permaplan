52872e418a7afcdbff7282a59ffdc092f78bafd6
stuartstaniford <stuart@earlywarn.org>
2022-02-23 07:47:38 -0500

    Partial implementation of BILFile::readHdrFile.

--- src/BILFile.cpp
+++ src/BILFile.cpp
@@ -7,6 +7,9 @@
 // https://webhelp.esri.com/arcgisdesktop/9.3/index.cfm?topicname=BIL,_BIP,_and_BSQ_raster_files
 
 #include "BILFile.h"
+#include "Global.h"
+#include <string.h>
+#include <err.h>
 
 
 // =======================================================================================
@@ -31,7 +34,69 @@ BILFile::~BILFile(void)
 
 bool BILFile::readHdrFile(char* path, char* stub)
 {
+  char fileName[256];
+  char line[128];
+  FILE* file;
   
+  // Figure out the file name and open the file.
+  snprintf(fileName, 256, "%s/%s.hdr", path, stub);
+  file = fopen(fileName, "r");
+  unless(file)
+    err(-1, "Couldn't open %s.\n", fileName);
+  
+  // Loop over the lines in the file
+  while(fgets(line, 128, file))
+   {
+    if( (strncmp(line, "BYTEORDER", 9) == 0) || (strncmp(line, "byteorder", 9) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "LAYOUT", 6) == 0) || (strncmp(line, "layout", 6) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "NROWS", 5) == 0) || (strncmp(line, "nrows", 5) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "NCOLS", 5) == 0) || (strncmp(line, "ncols", 5) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "NBANDS", 6) == 0) || (strncmp(line, "nbands", 6) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "NBITS", 5) == 0) || (strncmp(line, "nbits", 5) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "BANDROWBYTES", 12) == 0) || (strncmp(line, "bandrowbytes", 12) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "TOTALROWBYTES", 13) == 0) || (strncmp(line, "totalrowbytes", 13) == 0))
+     {
+      
+     }
+
+    if( (strncmp(line, "BANDGAPBYTES", 12) == 0) || (strncmp(line, "bandgapbytes", 12) == 0))
+     {
+      
+     }
+    
+   }
+  
+  // Clean up and go home.
+  fclose(file);
+  return false;
 }
 
 
