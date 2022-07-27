f7718bdda7d42fbb94b912aa0abca9a81b8911e9
Stuart Staniford <stuart@earlywarn.org>
2022-07-27 08:13:45 -0400

    Stub out DynamicallyTypable::writeJson

--- src/DynamicallyTypable.cpp
+++ src/DynamicallyTypable.cpp
@@ -5,4 +5,16 @@
 
 #include "DynamicallyTypable.h"
 
+
+// =======================================================================================
+/// @brief Provide json objects which a receiver can parse based on the dynamic type.
+/// @param buf The place in memory to output the JSON text to.
+/// @param bufSize The size of the buffer that can safely be written to.
+
+int DynamicallyTypable::writeJson(char* buf, unsigned bufSize)
+{
+  return 0;
+}
+
+
 // =======================================================================================
