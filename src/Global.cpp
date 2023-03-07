0aec16d570528d517c46cdbcff8caa0f2038e668
stuartstaniford <stuart@earlywarn.org>
2023-03-07 06:40:22 -0500

    Added hexdump() to Global.cpp

--- src/Global.cpp
+++ src/Global.cpp
@@ -43,6 +43,32 @@ void initGlobals(int nThreads)
 
 
 // =======================================================================================
+// Utility function for pretty-printing binary strings (eg in logs).
+// Originally sourced from:
+// https://stackoverflow.com/questions/29242/off-the-shelf-c-hex-dump-code
+
+void hexdump(void *ptr, int buflen) 
+{
+  unsigned char *buf = (unsigned char*)ptr;
+  int i, j;
+  for (i=0; i<buflen; i+=16) 
+   {
+    printf("%06x: ", i);
+    for (j=0; j<16; j++) 
+      if (i+j < buflen)
+        printf("%02x ", buf[i+j]);
+      else
+        printf("   ");
+    printf(" ");
+    for (j=0; j<16; j++) 
+      if (i+j < buflen)
+        printf("%c", isprint(buf[i+j]) ? buf[i+j] : '.');
+    printf("\n");
+   }
+}
+
+
+// =======================================================================================
 // Extract a ':' separated vector from a (URL) string.
 
 #define NUM_BUF_SIZE 32
