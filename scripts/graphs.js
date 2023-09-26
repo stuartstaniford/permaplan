3e3798f51cb254e8d236d160241641d55f3e6245
stuartstaniford <stuart@earlywarn.org>
2023-09-26 10:34:01 -0400

    Add fetchAndProcessStream to graphs.js

--- scripts/graphs.js
+++ scripts/graphs.js
@@ -22,10 +22,13 @@ function scatterPlot(svgIdName, data)
   // Compute the scale domains dynamically
   const {xDomain, yDomain} = computeScaleDomains(data);
 
+  console.log("xDomain for " + svgIdName + " is " + xDomain + ".\n");
+  console.log("yDomain for " + svgIdName + " is " + yDomain + ".\n");
+  
   // Set the computed domains to the scales
   const xScale = d3.scaleLinear().domain(xDomain).range([0, width]);
   const yScale = d3.scaleLinear().domain(yDomain).range([height, 0]);
-
+  
   // Axis
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
@@ -82,8 +85,10 @@ function clearSVG(svgIdName)
 // =======================================================================================
 /// @brief Fetches tab delimited data from a URL and processes it into an array.
 /// 
+/// The series are in columns, and the first column is the x values, and the subsequent
+/// columns are the y-values for each case.
 /// @returns The array of data
-/// @param url The id of the svg that the graph should be drawn in.
+/// @param url The url from which to fetch the data.
 
 async function fetchData(url) 
 {
@@ -92,27 +97,28 @@ async function fetchData(url)
   const text = await response.text();
 
   // Split the text data by lines and then by tabs
-  const lines = text.split('\n').map(line => line.split('\t'));
-  const header = lines[0];
-    
-  const dataMap = new Map();
+  const lines = text.split('\n').map(line => line.trim().split('\t'));
+  const headers = lines[0];  // Assuming the first row contains headers
 
-  // Process each line (skip the header)
-  for (let i = 1; i < lines.length; i++) 
-   {
-    const [series, x, y] = lines[i];
-        
-    // If the series doesn't exist in our map, add it
-    if (!dataMap.has(series)) 
-     {
-      dataMap.set(series, { series, values: [] });
-     }
+  // Transpose the matrix to work with columns (series) instead of rows
+  const transposed = headers.map((_, i) => lines.map(row => row[i]));
 
-    // Push the x and y values to the series' values array
-    dataMap.get(series).values.push({ x: +x, y: +y }); // '+' is used to convert string to number
-    }
+  const data = [];
+  for (let i = 1; i < headers.length; i++) {  // Skip the first column, assuming it contains x-values
+    const seriesName = headers[i];
+    const seriesData = transposed[i].slice(1).map((y, j) => ({
+      x: +transposed[0][j + 1],  // +1 to skip header
+      y: +y
+    }));
+    console.log("Series name is " + seriesName + "\n");
+    console.log("Series data is " + seriesData + "\n");
+    data.push({
+      series: seriesName,
+      values: seriesData
+    });
+  }
 
-  return Array.from(dataMap.values());
+  return data;
 }
 
 
@@ -124,7 +130,7 @@ async function fetchData(url)
 
 function getStationIdFromCurrentPath()
 {
-  var pathComponents = window.location.pathname.split('/');
+  const pathComponents = window.location.pathname.split('/');
   return pathComponents[pathComponents.length - 1];
 }
 
@@ -179,6 +185,50 @@ function scatterPlotObservable(svgIdName, observable)
               console.error(`Error occurred fetching data from ${url}: ${error}`);
               alert(`Error occurred fetching data from ${url}: ${error}`);
             });
+}
+
+
+// =======================================================================================
+/// @brief Fetch data from a url and process it in a line oriented way as it arrives.
+///
+/// @param url The url from whence to get the data
+/// @param processLine A function which takes one line and does something with it.
+/// Example Usage
+/// fetchAndProcessStream('your_url_here', line => {
+///    console.log(line);  // or any other processing
+/// });
+
+async function fetchAndProcessStream(url, processLine) 
+{
+  const response = await fetch(url);
+  const reader = response.body.getReader();
+  const textDecoder = new TextDecoder('utf-8');
+  let lastPartialLine = '';
+
+  while (true) 
+   {
+    // Get a chunk of the response body (eg one packet's worth) and decode it
+    const { done, value } = await reader.read();
+    if (done) 
+     {
+      break;
+     }
+    let textChunk = textDecoder.decode(value, { stream: true });
+    
+    let lines = textChunk.split('\n');
+    lines[0] = lastPartialLine + lines[0];
+    lastPartialLine = lines.pop(); // save partial line for the next chunk
+        
+    for (let line of lines) 
+     {
+      processLine(line);
+     }
+   }
+
+  if (lastPartialLine) 
+   {
+    processLine(lastPartialLine);
+   }
 }
 
 
